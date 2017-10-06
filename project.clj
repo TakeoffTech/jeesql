baca2facb469ca3b5f1cd443b52ff089534a8ab5
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-10-06 18:42:22 +0300

    Update core.async dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.6.2-alpha3"]
-                 [org.clojure/core.async "0.2.385"]]
+                 [org.clojure/core.async "0.3.443"]]
   ;:pedantic? :abort
   :scm {:name "git"
         :url "https://github.com/tatut/jeesql"}
